fa08cff27986a8d0442b933a329f273975927655
Jimenatapia <jtapia@escuelasproa.edu.ar>
2025-12-01 12:27:19 -0300

    base de datos, registro de usuarios, jime

---
+++ jime/basededatos.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE registro_usuarios;
+USE registro_usuarios;
+
+CREATE TABLE usuarios (
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   nombre VARCHAR(100),
+   email VARCHAR(100) UNIQUE,
+   Password VARCHAR(100),
+   edad INT,
+   ciudad VARCHAR(100)
+);
+
+insert into usuarios (nombre,email,password,edad,ciudad) values ('juancito', 'juan@gmail.com','hoola', 84,'lice' );
+insert  into usuarios (nombre,email,password,edad,ciudad) values ('bianca', 'bianca@gmail.com' , 'caracol', 23, 'alem' ) ;
