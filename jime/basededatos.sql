4c2d300150c43f2ea24eb596babc6e76f51b5199
brotosyntesis <pipee3000@gmail.com>
2025-12-04 00:17:09 -0300

    Agrego la parte de eliminación del usuario o datos personales y modificar datos del usuario

--- jime/basededatos.sql
+++ jime/basededatos.sql
@@ -10,5 +10,11 @@ CREATE TABLE usuarios (
    ciudad VARCHAR(100)
 );
 
-insert into usuarios (nombre,email,password,edad,ciudad) values ('juancito', 'juan@gmail.com','hoola', 84,'lice' );
-insert  into usuarios (nombre,email,password,edad,ciudad) values ('bianca', 'bianca@gmail.com' , 'caracol', 23, 'alem' ) ;
+insert into usuarios (nombre,email,password,edad,ciudad) values ('juancito', 'juan@gmail.com','hoola', 84,'liceo' );
+insert  into usuarios (nombre,email,password,edad,ciudad) values ('bianca', 'bianca@gmail.com' , 'caracol', 23, 'alem' );
+
+UPDATE usuarios SET ciudad = NULL WHERE email = 'bianca@gmail.com';
+DELETE FROM usuarios WHERE email = 'bianca@gmail.com';
+
+UPDATE usuarios SET ciudad = 'Río Cuarto' WHERE email = 'juan@gmail.com';
+UPDATE usuarios SET nombre = 'Juan Nuñez', edad = 33 WHERE email = 'juan@gmail.com';
