1de0c28859fbd648991fb8c15651107323efa108
brotosyntesis <pipee3000@gmail.com>
2025-12-04 22:02:55 -0300

    lo actualizo

--- jime/basededatos.sql
+++ jime/basededatos.sql
@@ -12,9 +12,3 @@ CREATE TABLE usuarios (
 
 insert into usuarios (nombre,email,password,edad,ciudad) values ('juancito', 'juan@gmail.com','hoola', 84,'liceo' );
 insert  into usuarios (nombre,email,password,edad,ciudad) values ('bianca', 'bianca@gmail.com' , 'caracol', 23, 'alem' );
-
-UPDATE usuarios SET ciudad = NULL WHERE email = 'bianca@gmail.com';
-DELETE FROM usuarios WHERE email = 'bianca@gmail.com';
-
-UPDATE usuarios SET ciudad = 'Río Cuarto' WHERE email = 'juan@gmail.com';
-UPDATE usuarios SET nombre = 'Juan Nuñez', edad = 33 WHERE email = 'juan@gmail.com';
